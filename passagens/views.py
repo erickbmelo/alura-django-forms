ecce988c8824275f8a79f7bb0ddf156d1e01affb
erick.melo <erick.melo@sorocaba.sp.gov.br>
2023-02-02 15:28:09 -0300

    Pegando os dados do formulário e exibindo em outra página

--- passagens/views.py
+++ passagens/views.py
@@ -4,3 +4,8 @@ from passagens.forms import PassagemForms
 def index(request):
     form = PassagemForms()
     return render(request, 'index.html', {'form': form})
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST)
+        return render(request, 'minha_consulta.html', {'form': form})
