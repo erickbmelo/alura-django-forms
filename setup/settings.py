50a3d67cab67841663dc18e1a1b2d17f07397915
erick.melo <erick.melo@sorocaba.sp.gov.br>
2023-02-02 15:56:15 -0300

    Implementando o tempus_dominus

--- setup/settings.py
+++ setup/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tempus_dominus',
     'passagens',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPUS_DOMINUS_LOCALIZE = True
