89ef83daab21002a633fbd6660c8a22961057e72
erickbmelo <erick.melo@sorocaba.sp.gov.br>
2023-02-03 10:59:57 -0300

    Validações de todos os campos com método clean

--- passagens/views.py
+++ passagens/views.py
@@ -9,3 +9,11 @@ def revisao_consulta(request):
     if request.method == 'POST':
         form = PassagemForms(request.POST)
         return render(request, 'minha_consulta.html', {'form': form})
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST)
+        if form.is_valid():
+            return render(request, 'minha_consulta.html', {'form':form})
+        else:
+            return render(request, 'index.html', {'form':form})
